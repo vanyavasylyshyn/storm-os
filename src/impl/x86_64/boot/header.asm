30294cfc59f4d452e7774535271a51bb4fd17f64
Ivan Vasylyshyn <ivan.vasylyshyn@digit.az>
2024-11-07 13:58:38 +0200

    Add 64bit mode, kernel stack, virtual memory tables, ability to use c code

--- src/impl/x86_64/boot/header.asm
+++ src/impl/x86_64/boot/header.asm
@@ -4,13 +4,13 @@ header_start:
   dd 0xe85250d6 ; multiboot2
   ; architecture
   dd 0 ; protected mode i386
-  ; header lenght
+  ; header length
   dd header_end - header_start
-  ; checksum 
+  ; checksum
   dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
   ; ned tag
   dw 0
-  dw 0 
+  dw 0
   dd 8
 header_end:
